6c80ed40c643c8615a5cf67330b36a7f60dee4c9
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2023-12-12 15:50:38 +0100

    first iteration

---
+++ src/components/TravelList.jsx
@@ -0,0 +1,27 @@
+import travelPlansData from "../assets/travel-plans.json";
+import "../App.css";
+
+function TravelList() {
+  return (
+    <div className="listCtn">
+      {travelPlansData.map((plan) => (
+        <div key={plan.id} className="container">
+          <div className="imageCtn">
+            <img src={plan.image} alt="image" />
+          </div>
+          <div className="textCtn">
+            <h3>
+              {plan.destination} ({plan.days} Days)
+            </h3>
+            <p>{plan.description}</p>
+            <p>
+              <strong>Price:</strong> {plan.totalCost}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TravelList;
